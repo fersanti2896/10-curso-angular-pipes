f0848ae42e14ea4a59a6efdf6e04d2d600cae672
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-03 15:30:47 -0600

    Creando componente basicos, no-comunes, numeros, ordenar.component y app-router.module donde almacenan las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module'
+import { VentasModule } from './ventas/ventas.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,7 @@ import { SharedModule } from './shared/shared.module'
   ],
   imports: [
     BrowserModule,
+    AppRouterModule,
     SharedModule
   ],
   providers: [],
