df25ce6fefa356aa208b36bc010e351c98b1acfa
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-03 19:07:06 -0600

    Cambiando el idioma por defecto en app.module de manera global

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module'
 import { VentasModule } from './ventas/ventas.module';
+
+/* Cambiar el local de la app, es decir, el idioma por defecto */
+import localEsMX from '@angular/common/locales/es-MX';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localEsMX);
 
 @NgModule({
   declarations: [
@@ -16,7 +22,9 @@ import { VentasModule } from './ventas/ventas.module';
     SharedModule, 
     VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-MX' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
