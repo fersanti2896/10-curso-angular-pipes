2fb67a82c34aa2576a80b86a12e0017be560f1af
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-04 01:34:07 -0600

    Haciendo uso de AsyncPipe en no-comunes.component

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -8,7 +9,7 @@ import { Component } from '@angular/core';
 })
 export class NoComunesComponent {
   /* i18nSelect */
-  nombre: string = 'Aline';
+  nombre: string = 'Aline Rojas';
   genero: string = 'femenino';
   invitacionMap = {
     'masculino': 'invitarlo', 
@@ -55,4 +56,12 @@ export class NoComunesComponent {
       vuela: false
     }
   ];
+
+  /* Async Pipe */
+  miObservable = interval(1000);
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data de promesa!')
+    }, 3500);
+  });
 }
